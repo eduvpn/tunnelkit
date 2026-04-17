bbf6d7de8a5c69894bb5f91ccf8dc2440c3da1c4
Roopesh Chander <roop@roopc.net>
2026-04-17 17:07:22 +0530

    Update openssl-apple in Package.swift

--- Package.swift
+++ Package.swift
@@ -39,7 +39,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver", from: "1.9.0"),
-        .package(url: "https://codeberg.org/roopc/openssl-apple", .exact("3.5.404")),
+        .package(url: "https://codeberg.org/eduVPN/openssl-apple", .exact("3.5.601")),
         .package(name: "WireGuardKit", url: "https://git.zx2c4.com/wireguard-apple", .exact("1.0.15-26"))
 //        .package(name: "WireGuardKit", url: "https://github.com/passepartoutvpn/wireguard-apple", from: "1.0.16")
     ],
